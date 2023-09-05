81f9b6057dfc47abe9ad6f7cf54d9e21f276cb15
hWeitian <huangweitian1@hotmail.com>
2023-09-05 15:12:01 +0800

    Add export invoice data function

--- controllers/insertionOrder.js
+++ controllers/insertionOrder.js
@@ -1,4 +1,4 @@
-const db = require("../db/models/index");
+const { db } = require("../db/models/index");
 const { Op } = require("sequelize");
 
 const { InsertionOrder, Order, Company, Contact, Product, Magazine } = db;
