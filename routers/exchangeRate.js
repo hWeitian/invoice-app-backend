5f1bace9942ef5f071e07063c1808f1864f0bba6
hWeitian <huangweitian1@hotmail.com>
2023-09-06 16:09:05 +0800

    Update get exchange rate function for invoices

--- routers/exchangeRate.js
+++ routers/exchangeRate.js
@@ -5,7 +5,7 @@ const exchangeRates = require("../controllers/exchangeRate");
 const router = express.Router();
 
 router.get("/", exchangeRates.getAll);
-router.get("/latest", exchangeRates.getOneLatest);
+router.get("/latest/:month/:year", exchangeRates.getRate);
 router.get("/:page/:size", exchangeRates.getTableData);
 router.post("/", exchangeRates.addRate);
 router.put("/:id", exchangeRates.updateRate);
